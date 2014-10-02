2a358b26aca70fdc9165250e3aa4528cb7421f4f
Andrew Sinner <andrewsinner@gmail.com>
2014-10-02 16:00:49 -0700

    Updated tests

--- test/features/foundation_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-feature 'Foundation' do
-  scenario 'site implements Zurb Foundation' do
-    visit root_url
-    page.must_have_xpath('//link[@href="foundation"]')
-  end
-end
